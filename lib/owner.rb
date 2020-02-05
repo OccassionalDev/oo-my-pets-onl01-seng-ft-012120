1d2818fd4e811eeadc2c6db6682c3661d1fd2619
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-05 16:04:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
 class Owner
   # code goes here
+  attr_reader :name, :species 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    save
+  end 
+  
+  def save 
+    @@all << self
+  end 
+  
+  def self.all 
+    return @@all 
+  end 
+  
+  def say_species
+    return "I am a #{self.species}."
+  end 
+  
+  def self.count 
+    return self.all.count
+  end 
+  
+  def self.reset_all
+    self.all.clear
+  end 
+    
 end
